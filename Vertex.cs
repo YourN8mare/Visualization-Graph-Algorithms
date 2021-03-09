1ddec46abc6ab3f571d9d91287171b3d91844e19
YourN8mare <72352704+YourN8mare@users.noreply.github.com>
2021-03-09 15:20:40 +0300

    Add files via upload

---
+++ Vertex.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+
+namespace GraphAlgorithms
+{
+    public class Vertex
+    {
+        public Pen color = new Pen(Color.Black);
+        public Vertex(int x, int y, int number)
+        {
+            X = x;
+            Y = y;
+            Number = number;
+        }
+
+        public int X { get; }
+
+        public int Y { get; }
+
+        public int Number { get; }
+    }
+}
