cd1d8559f125426aa890383ccfefec991f89e532
YourN8mare <72352704+YourN8mare@users.noreply.github.com>
2021-03-09 15:22:17 +0300

    Add files via upload

---
+++ Edge.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+
+namespace GraphAlgorithms
+{
+    public class Edge
+    {
+        public Pen color = new Pen(Color.DarkGoldenrod);
+
+        public Edge(Vertex v1, Vertex v2, string weight)
+        {
+            V1 = v1;
+            V2 = v2;
+            Weight = weight;
+        }
+
+        public Vertex V1 { get; }
+
+        public Vertex V2 { get; }
+
+        public string Weight { get; set; }
+    }
+}
